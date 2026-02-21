ac86c9976bb22f80532116528b239f1bffd107c2
Smartbilling <smartbillingpost@gmail.com>
2026-02-21 22:57:17 +0530

    zoom change

--- js/gallery.js
+++ js/gallery.js
@@ -6,11 +6,12 @@ fetch("gallery.txt")
 
     images.forEach(img => {
       const div = document.createElement("div");
-      div.className = "overflow-hidden rounded-xl shadow hover:shadow-lg";
+      div.className = "overflow-hidden rounded-xl shadow hover:shadow-lg bg-white";
 
       div.innerHTML = `
         <img src="images/${img.trim()}" 
-class="w-full h-64 object-contain hover:scale-105 transition duration-300 bg-white";
+        class="w-full h-64 object-contain hover:scale-105 transition duration-300 bg-white">
+      `;
 
       gallery.appendChild(div);
     });
